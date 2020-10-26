c7dd1aa5c4b1db2b5b5fb1cd24e2544fa6d4496c
Keshav Anand <keshav.anand@in.ibm.com>
2020-10-26 08:49:16 +0530

    Testing another change in console.log

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function (req,res) {
-  res.send('Bonjour Mars!\n');
+  res.send('Bonjour Mars! Salut \n');
 });
 
 app.listen(8080, function () {
